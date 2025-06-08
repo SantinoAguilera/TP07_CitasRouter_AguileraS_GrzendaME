8ed68fd34b90c2cfda30be8e37cb900322f11b4c
SantinoAguilera <48313116@est.ort.edu.ar>
2025-06-08 19:30:50 -0300

    Capitalism

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router';
 import './Home.css'
+import Titulo from '../Titulo'
 
 export default function Home() {
   return (
-    <div>
-      {/*<Link to={"/MisCitas"}>Mis Citas</Link>*/}
+    <div className='Home'>
+      <Titulo titulo="Sistema de Gestión de Citas" />
+      <div className='HomeBody'>
+        <Link to={"/MisCitas"}>Mis Citas</Link>
+        <Link to={"/NuevasReservas"}>Reservar</Link>
+      </div>
     </div>
   )
 }
